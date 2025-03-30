a0bb163e2865d5bb37a2e73d49c9ba050ee669b9
Nicolas <johanbecerra000@gmail.com>
2025-03-30 18:50:55 -0500

    Implementacion de actualizacion de clientes

--- infrastructure/ConexionDB.go
+++ infrastructure/ConexionDB.go
@@ -5,7 +5,7 @@ import (
 	"gorm.io/gorm"
 )
 
-const dns string = "host=localhost user=postgres password=Davidfelipe2017 dbname=labgestor sslmode=disable"
+const dns string = "host=localhost user=postgres password=password dbname=labgestor sslmode=disable"
 
 func NewConexionDB() (*gorm.DB, error) {
 	conexion, err := gorm.Open(postgres.Open(dns), &gorm.Config{})
--- internal/controllers/cliente.go
+++ internal/controllers/cliente.go
@@ -11,6 +11,7 @@ import (
 // Interfaz que define los metodos del controlador
 type ClienteController interface {
 	CrearCliente(c echo.Context) error
+	ActualizarCliente(c echo.Context) error
 }
 
 // Structura que conecte con el repositorio
@@ -29,7 +30,7 @@ func NewClienteController(repo repository.ClienteRepository) ClienteController {
 func (controller clienteController) CrearCliente(c echo.Context) error {
 	// Se lee el cuerpo del request
 	var requestBody struct {
-		Nombres   string
+		Nombre    string
 		Direccion string
 	}
 
@@ -38,10 +39,16 @@ func (controller clienteController) CrearCliente(c echo.Context) error {
 	}
 
 	// TODO: Realizar validaciones de campos
+	if requestBody.Nombre == "" {
+		return c.JSON(http.StatusBadRequest, map[string]string{"message": "El campo 'Nombre' es obligatorio"})
+	}
+	if requestBody.Direccion == "" {
+		return c.JSON(http.StatusBadRequest, map[string]string{"message": "El campo 'Direccion' es obligatorio"})
+	}
 
 	// Crear una instancia del modelo
 	cliente := models.Cliente{
-		Nombres:   requestBody.Nombres,
+		Nombre:    requestBody.Nombre,
 		Direccion: requestBody.Direccion,
 	}
 
@@ -52,4 +59,46 @@ func (controller clienteController) CrearCliente(c echo.Context) error {
 
 	// Si todo sale bien se retorna un estado de 200
 	return c.JSON(http.StatusOK, map[string]string{"message": "Se registro el cliente con exito"})
+}
+
+func (controller clienteController) ActualizarCliente(c echo.Context) error {
+	var requestBody struct {
+		ID        int
+		Nombre    string
+		Direccion string
+	}
+	if err := c.Bind(&requestBody); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"message": "No se pudo leer el cuerpo del request", "error": err.Error()})
+	}
+
+	// Realizar validaciones de campos
+	if requestBody.Nombre == "" {
+		return c.JSON(http.StatusBadRequest, map[string]string{
+			"message": "El campo 'Nombre' es obligatorio",
+		})
+	}
+	if requestBody.Direccion == "" {
+		return c.JSON(http.StatusBadRequest, map[string]string{
+			"message": "El campo 'Direccion' es obligatorio",
+		})
+	}
+
+	// Crear una instancia del modelo
+	cliente := models.Cliente{
+		ID:        requestBody.ID,
+		Nombre:    requestBody.Nombre,
+		Direccion: requestBody.Direccion,
+	}
+
+	// Llamamos al repositorio para actualizar el Cliente en la base de datos
+	if err := controller.Repo.ActualizarCliente(&cliente); err != nil {
+		// Si hay un error al actualizar, lo retornamos con un mensaje adecuado
+		return c.JSON(http.StatusInternalServerError, map[string]string{
+			"message": "No se pudo actualizar el cliente",
+			"error":   err.Error(),
+		})
+	}
+
+	// Si todo salió bien, respondemos con un estado 200 y un mensaje de éxito
+	return c.JSON(http.StatusOK, map[string]string{"message": "Se actualizo el cliente con exito"})
 }
