d27baf10dda6734e97f2a11da257fdcafd111ae4
Nicolas <johanbecerra000@gmail.com>
2025-04-02 19:43:26 -0500

    Implementacion para obtener un cliente por ID y listar todos los clientes

--- internal/routes/cliente.go
+++ internal/routes/cliente.go
@@ -14,9 +14,11 @@ type clienteHandler struct {
 // Funcion que instancia el hanlder
 func NewClienteHandler(e *echo.Echo, controller controllers.ClienteController) {
 	// Instanciar el handler
-	hanlder := clienteHandler{Controller: controller}
+	handler := clienteHandler{Controller: controller}
 
 	// Definir EndPoints (Puntos de entrada a la API)
-	e.POST("/clientes/crearCliente", hanlder.Controller.CrearCliente)
-	e.POST("/clientes/actualizarCliente", hanlder.Controller.ActualizarCliente)
+	e.POST("/clientes/crearCliente", handler.Controller.CrearCliente)
+	e.POST("/clientes/actualizarCliente", handler.Controller.ActualizarCliente)
+	e.GET("/clientes/obtenerCliente/:id", handler.Controller.ObtenerCliente)
+	e.GET("/clientes/obtenerClientes", handler.Controller.ObtenerClientes)
 }
