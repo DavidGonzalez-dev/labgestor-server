66afa6dd28b7ef6879f3f62d0cf2d1fb2cdecf21
DavidFelipe-dev <davidfgonzalezc1@gmail.com>
2025-04-08 18:40:39 -0500

    Update internal/routes/usuario.go

--- internal/routes/usuario.go
+++ internal/routes/usuario.go
@@ -26,6 +26,6 @@ func NewUsuarioHanlder(e *echo.Echo, controller controllers.UsuarioController, r
 	// -----------------------------------
 	e.POST("/usuarios/registrar", controller.RegistrarUsuario) // Registrar Usuario en el sistema
 	e.GET("/usuarios/:id", controller.ObtenerPerfil, middleware.RequireAuth(repo, "admin")) // Obtener la info de un usuario
-	e.DELETE("/usuarios/:id", controller.DeshabilitarUsuario) // Dehabilitar el Ingreso de un Usuario
+	e.DELETE("/usuarios/:id", controller.DeshabilitarUsuario, middleware.RequireAuth(repo, "admin")) // Dehabilitar el Ingreso de un Usuario
 }	
 
