5adeb072ad90941e7babd4abcd5b3521e6a05c1c
DavidFelipe-dev <davidfgonzalezc1@gmail.com>
2025-04-18 21:04:27 -0500

    REFACTOR: Endpoints CRUD Productos

--- internal/models/usuario.go
+++ internal/models/usuario.go
@@ -8,13 +8,26 @@ type rolUsuario struct {
 
 // Declaracion del modelo de referencia de la tabla usuarios
 type Usuario struct {
-	ID         string `gorm:"primaryKey"` // Llave primaria
-	Nombres    string
-	Apellidos  string
-	Correo     string
-	Contrasena string
-	Firma      string
-	Estado     bool
+	ID         string     `gorm:"primaryKey,omitempty"` // Llave primaria
+	Nombres    string     `json:"nombres,omitempty"`
+	Apellidos  string     `json:"apellidos,omitempty"`
+	Correo     string     `json:"correo,omitempty"`
+	Contrasena string     `json:"contrasena,omitempty"`
+	Firma      string     `json:"firma,omitempty"`
+	Estado     bool       `json:"estado,omitempty"`
 	RolID      int        `json:"-"`                 // Llave Secundaria
-	Rol        rolUsuario `gorm:"foreignKey: RolID"` // Referecia a tabla llave foranea
+	Rol        *rolUsuario `gorm:"foreignKey: RolID" json:"rol,omitempty"` // Referecia a tabla llave foranea
+}
+
+func (usuario Usuario) ToMap() map[string]any {
+	return map[string]any{
+		"ID":         usuario.ID,
+		"Nombres":    usuario.Nombres,
+		"Apellidos":  usuario.Apellidos,
+		"Correo":     usuario.Correo,
+		"Contrasena": usuario.Contrasena,
+		"Firma":      usuario.Firma,
+		"Estado":     usuario.Estado,
+		"RolID":      usuario.RolID,
+	}
 }
