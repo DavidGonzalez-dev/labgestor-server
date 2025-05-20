0439870ce3c80daf15acd0e73da4ab8834bca9aa
NicolasBece <167639636+NicolasBece@users.noreply.github.com>
2025-05-19 21:17:18 -0500

    Feat insertar pruebas de recuento (#13)
    * UPDATE: Actualizacion de la estructura json del rol del usuario
    
    * UPDATE: Se actualiza la estructura json que de los usuarios
    
    * BUGFIX: Se arregla validacion de campos para actualizar los usuarios
    
    * Insercion de Pruebas Recuento
    
    * UPDATE: Funcion de validacion de campos para aceptar campos opccionales
    
    * BUGFIX: Se arregla el error de formato de fechas al actualizar el registro de entrada de productos
    
    * FEAT: Implementacion reglas de validacion para pruebas de recuento
    
    ---------
    
    Co-authored-by: DavidFelipe-dev <davidfgonzalezc1@gmail.com>

---
+++ internal/repository/pruebasRecuento.go
@@ -0,0 +1,26 @@
+package repository
+
+import (
+	"labgestor-server/internal/models"
+
+	"gorm.io/gorm"
+)
+
+type PruebaRecuentoRepository interface {
+	CrearPruebaRecuento(pruebaRecuento *models.PruebaRecuento) error
+}
+
+type pruebaRecuentoRepository struct {
+	DB *gorm.DB
+}
+
+func NewPruebaRecuentoRepository(db *gorm.DB) PruebaRecuentoRepository {
+	return &pruebaRecuentoRepository{DB: db}
+}
+
+// Este metodo nos permite crear un registro de una prueba de recuento en la base de datos
+func (repo *pruebaRecuentoRepository) CrearPruebaRecuento(pruebaRecuento *models.PruebaRecuento) error {
+
+	// Se crea el producto y se verifica que no hallan errores
+	return repo.DB.Create(&pruebaRecuento).Error
+}
