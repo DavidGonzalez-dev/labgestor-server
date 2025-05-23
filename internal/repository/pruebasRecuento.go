977b6b78b8a105fd9d6c4e9d570436b1cb633baf
DavidFelipe-dev <davidfgonzalezc1@gmail.com>
2025-05-23 12:34:04 -0500

    REFACTOR: Se hace refactorizacion de la capa del repositorio y controlador

--- internal/repository/pruebasRecuento.go
+++ internal/repository/pruebasRecuento.go
@@ -6,6 +6,8 @@ import (
 	"gorm.io/gorm"
 )
 
+
+// En esta interfaz se definene los metodos del repositorio
 type PruebaRecuentoRepository interface {
 	CrearPruebaRecuento(pruebaRecuento *models.PruebaRecuento) error
 }
@@ -14,13 +16,20 @@ type pruebaRecuentoRepository struct {
 	DB *gorm.DB
 }
 
+// Esta funcion nos permite instanciar el repositorio
+// y recibir la base de datos como parametro
 func NewPruebaRecuentoRepository(db *gorm.DB) PruebaRecuentoRepository {
 	return &pruebaRecuentoRepository{DB: db}
 }
 
+
+// ? ------------------------------------------------
+// ? CRUD PRUEBAS DE RECUEENTO
+// ? ------------------------------------------------
+
 // Este metodo nos permite crear un registro de una prueba de recuento en la base de datos
 func (repo *pruebaRecuentoRepository) CrearPruebaRecuento(pruebaRecuento *models.PruebaRecuento) error {
-
-	// Se crea el producto y se verifica que no hallan errores
+	// Se crea el producto y se retorna el error
+	// en caso de que ocurra un error
 	return repo.DB.Create(&pruebaRecuento).Error
 }
