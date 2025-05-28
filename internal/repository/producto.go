c3e96e60e1f144f4ce325fcf6a7e6dadf13f7332
DavidFelipe-dev <davidfgonzalezc1@gmail.com>
2025-05-28 13:24:21 -0500

    UPDATE: Compatibilidad con navegadores basados en firefox

--- internal/repository/producto.go
+++ internal/repository/producto.go
@@ -45,7 +45,10 @@ func (repo *productoRepository) ObtenerProductoID(numeroRegistroProducto string)
 		Preload("Producto.Cliente").                                 // Preload para que aparezca la informacion del cliente del producto
 		Preload("Producto.Fabricante").                              // Preload para que aparezca la informacion del fabricante del producto
 		Preload("Producto.TipoProducto").                            // Preload para que aparezca la informacion del tipo del producto
-		Preload("Producto.EstadoProducto").                          // Preload para que aparezca la informacion del estado del producto
+		Preload("Producto.EstadoProducto").
+		Preload("Usuario", func(db *gorm.DB) *gorm.DB{
+			return db.Select("id", "nombres", "apellidos")
+		}).                          // Preload para que aparezca la informacion del estado del producto
 		Where("numero_registro_producto=?", numeroRegistroProducto). // Filtro para seleccionar solo el registro que coincida con el numero de registro de producto pasado.
 		First(&entradaProducto).Error; err != nil {
 		// En caso de un error al momento de hacer la precarga de los datos se retorna el error y nil
