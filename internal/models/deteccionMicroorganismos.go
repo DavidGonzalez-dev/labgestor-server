cfb9a6ccee574aaec70e0460689b50151f7ffdbc
DavidFelipe-dev <davidfgonzalezc1@gmail.com>
2025-06-16 12:58:29 -0500

    UPDATE: Se añade el estado de la prueba de deteccion de microorganismo

--- internal/models/deteccionMicroorganismos.go
+++ internal/models/deteccionMicroorganismos.go
@@ -4,13 +4,14 @@ type DeteccionesMicroorganismos struct {
 	ID                     int    `gorm:"primaryKey autoincrement" json:"id"`
 	NombreMicroorganismo   string `json:"nombreMicroorganismo,omitempty"`
 	Especificacion         string `json:"especificacion,omitempty"`
-	Concepto               bool   `json:"concepto"`
+	Concepto               bool   `json:"concepto,omitempty"`
 	Tratamiento            string `json:"tratamiento,omitempty"`
 	MetodoUsado            string `json:"metodoUsado,omitempty"`
 	CantidadMuestra        string `json:"cantidadMuestra,omitempty"`
 	VolumenDiluyente       string `json:"volumenDiluyente,omitempty"`
-	Resultado              string `json:"resultado"`
+	Resultado              string `json:"resultado,omitempty"`
 	NumeroRegistroProducto string `json:"numeroRegistroProducto,omitempty"`
+	Estado                 string `json:"estado,omitempty"`
 }
 
 func (deteccionMicroorganismos DeteccionesMicroorganismos) ToMap() map[string]any {
