804762ad52b6753db202f67331717631747519f0
DavidFelipe-dev <davidfgonzalezc1@gmail.com>
2025-06-25 03:18:25 -0500

    Configuracion base de dato produccion

--- infrastructure/conexionDB.go
+++ infrastructure/conexionDB.go
@@ -1,6 +1,7 @@
 package infrastructure
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"time"
@@ -10,19 +11,25 @@ import (
 	"gorm.io/gorm/logger"
 )
 
-const dns string = "host=localhost user=postgres password=Davidfelipe2017 dbname=labgestor sslmode=disable"
-
 func NewConexionDB() (*gorm.DB, error) {
-    conexion, err := gorm.Open(postgres.Open(dns), &gorm.Config{
-        Logger: logger.New(
-            log.New(os.Stdout, "\r\n[GORM] ", log.LstdFlags),
-            logger.Config{ 
-                SlowThreshold:             time.Second,  // SQL que demoran más de 1s
-                LogLevel:                  logger.Info,   // Nivel de log (Error, Warn, Info)
-                IgnoreRecordNotFoundError: true,          // Ignorar "record not found"
-                Colorful:                  true,          // Colores en terminal
-            },
-        ),
-    })
-    return conexion, err
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_DATABASE")
+
+	dns := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=require", host, port, user, password, dbName)
+
+	conexion, err := gorm.Open(postgres.Open(dns), &gorm.Config{
+		Logger: logger.New(
+			log.New(os.Stdout, "\r\n[GORM] ", log.LstdFlags),
+			logger.Config{
+				SlowThreshold:             time.Second, // SQL que demoran más de 1s
+				LogLevel:                  logger.Info, // Nivel de log (Error, Warn, Info)
+				IgnoreRecordNotFoundError: true,        // Ignorar "record not found"
+				Colorful:                  true,        // Colores en terminal
+			},
+		),
+	})
+	return conexion, err
 }
